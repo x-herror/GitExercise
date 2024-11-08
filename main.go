d295ad87975942732b5cc6f04c3125d04f58ceb0
xherror-win <xherror@foxmail.com>
2024-11-08 19:30:42 +0800

    主干更新v1-feature0

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World!")
 	fmt.Println("主干更新v1")
+	fmt.Println("主干更新v1-feature0")
 	fmt.Println("主干更新v1-feature1")
 	fmt.Println("主干更新v1-feature2")
 	fmt.Println("主干更新v1-feature3")
